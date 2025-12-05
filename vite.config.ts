c4ae6b6183a09fb734bb4583f8adba1982df9edf
gromova-daria <146058791+gromova-daria@users.noreply.github.com>
2025-12-05 20:53:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: 'Harry_Potter_React-TS_try_deploy',
 })
