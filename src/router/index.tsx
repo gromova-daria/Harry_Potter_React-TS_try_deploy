f484e0cf5b06d51eee22d630aeedfb5d9ba9c1d8
gromova-daria <146058791+gromova-daria@users.noreply.github.com>
2025-12-05 21:51:40 +0300

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+import { HashRouter, Routes, Route } from "react-router-dom"
 import CharactersPage from "../pages/CharactersPage"
 import HomePage from "../pages/HomePage"
 import FilmographyPage from "../pages/FilmographyPage"
 import HousesPage from "../pages/HousesPage"
-
 export default function AppRouter() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/characters" element={<CharactersPage />} />
                 <Route path="/houses" element={<HousesPage />} />
                 <Route path="/filmography" element={<FilmographyPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
