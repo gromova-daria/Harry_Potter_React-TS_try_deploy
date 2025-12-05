baeb291535d6098b505caa946eed48d550a4ac53
LiliaDurnina <166434239+LiliaDurnina@users.noreply.github.com>
2025-12-05 16:54:56 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from 'App';
 
 import './styles/fonts.css';
 import './styles/reset.css';
